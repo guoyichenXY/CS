4c8063ae26468ce2214f437d1b4b76bd1736c2bf
guoyichenXY <150229219+guoyichenXY@users.noreply.github.com>
2023-12-05 15:14:19 +0800

    Add files via upload
    first commit

--- main.py
+++ main.py
@@ -1,167 +1,106 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-
-import socket
 import os
-import sys
 import struct
 import time
 import select
-import binascii
+import socket
 
 ICMP_ECHO_REQUEST = 8  # ICMP type code for echo request messages
 ICMP_ECHO_REPLY = 0  # ICMP type code for echo reply messages
-ICMP_Type_Unreachable = 11  # unacceptable host
-ICMP_Type_Overtime = 3  # request overtime
+ICMP_TimeExceeded = 11  # ICMP type code for time exceeded messages
 ID = 0  # ID of icmp_header
-SEQUENCE = 0  # sequence of ping_request_msg
+SEQUENCE = 0  # sequence of icmp packets
+MAX_HOPS = 30  # Maximum number of hops for traceroute
 
 
-def checksum(string):
+def checksum(data):
     csum = 0
-    countTo = (len(string) // 2) * 2
+    countTo = (len(data) // 2) * 2
     count = 0
-
     while count < countTo:
-        thisVal = string[count + 1] * 256 + string[count]
-        csum = csum + thisVal
-        csum = csum & 0xffffffff
-        count = count + 2
-
-    if countTo < len(string):
-        csum = csum + string[len(string) - 1]
-        csum = csum & 0xffffffff
-
+        thisVal = data[count + 1] * 256 + data[count]
+        csum += thisVal
+        csum &= 0xffffffff
+        count += 2
+    if countTo < len(data):
+        csum += data[len(data) - 1]
+        csum &= 0xffffffff
     csum = (csum >> 16) + (csum & 0xffff)
-    csum = csum + (csum >> 16)
+    csum += (csum >> 16)
     answer = ~csum
-    answer = answer & 0xffff
+    answer &= 0xffff
     answer = answer >> 8 | (answer << 8 & 0xff00)
-
-    answer = socket.htons(answer)
-
     return answer
 
 
-def receiveOnePing(icmpSocket, ID, timeout):
-    # 1. Wait for the socket to receive a reply
+def receiveOnePing(icmpSocket, ID, timeout, destAddr, ttl):
     timeBeginReceive = time.time()
     whatReady = select.select([icmpSocket], [], [], timeout)
     timeInRecev = time.time() - timeBeginReceive
     if not whatReady[0]:
-        return -1
+        return -1, ""
     timeReceived = time.time()
-    # 2. Once received, record time of receipt, otherwise, handle a timeout
     recPacket, addr = icmpSocket.recvfrom(1024)
-    # 3. Compare the time of receipt to time of sending, producing the total network delay
     byte_in_double = struct.calcsize("!d")
-    timeSent = struct.unpack("!d", recPacket[28: 28 + byte_in_double])[0]
-    totalDelay = timeReceived - timeSent
-    # 4. Unpack the packet header for useful information, including the ID
-    rec_header = recPacket[20:28]
-    replyType, replyCode, replyCkecksum, replyId, replySequence = struct.unpack('!bbHHh', rec_header)
-    # 5. Check that the ID matches between the request and reply
-    if ID == replyId and replyType == ICMP_ECHO_REPLY:
-        # 6. Return total network delay
-        return totalDelay
-    elif timeInRecev > timeout or replyType == ICMP_Type_Overtime:
-        return -3  # ttl overtime/timeout
-    elif replyType == ICMP_Type_Unreachable:
-        return -11  # unreachable
+    # Unpack the packet header for useful information, including the ID
+    ipHeader = recPacket[:20]
+    icmpHeader = recPacket[20:28]
+    icmp_type, icmp_code, icmp_checksum, icmp_packet_id, icmp_sequence = struct.unpack('!bbHHh', icmpHeader)
+    # Check if the reply packet is for us
+    if ID == icmp_packet_id:
+        return timeReceived - timeBeginReceive, addr[0]
+    elif timeInRecev > timeout:
+        return -2, ""
+    elif icmp_type == ICMP_TimeExceeded and icmp_code == 0:
+        return -3, addr[0]
     else:
-        print("request over time")
-        return -1
+        return -1, ""
 
 
-def sendOnePing(icmpSocket, destinationAddress, ID):
+def sendOnePing(icmpSocket, destinationAddress, ID, ttl):
     icmp_checksum = 0
-    # 1. Build ICMP header
     icmp_header = struct.pack('!bbHHh', ICMP_ECHO_REQUEST, 0, icmp_checksum, ID, SEQUENCE)
     time_send = struct.pack('!d', time.time())
-    # 2. Checksum ICMP packet using given function
     icmp_checksum = checksum(icmp_header + time_send)
-    # 3. Insert checksum into packet
     icmp_header = struct.pack('!bbHHh', ICMP_ECHO_REQUEST, 0, icmp_checksum, ID, SEQUENCE)
-    # 4. Send packet using socket
     icmp_packet = icmp_header + time_send
+    icmpSocket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, struct.pack('I', ttl))
     icmpSocket.sendto(icmp_packet, (destinationAddress, 80))
-    # 5. Record time of sending
-    pass  # Remove/replace when function is complete
 
 
-def doOnePing(destinationAddress, timeout):
-    # 1. Create ICMP socket
+def doOnePing(destinationAddress, timeout, ttl):
     icmpName = socket.getprotobyname('icmp')
-    icmp_Socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmpName)
-    # 2. Call sendOnePing function
-    sendOnePing(icmp_Socket, destinationAddress, ID)
-    # 3. Call receiveOnePing function
-    totalDelay = receiveOnePing(icmp_Socket, ID, timeout)
-    # 4. Close ICMP socket
-    icmp_Socket.close()
-    # 5. Return total network delay
-    return totalDelay
-    pass  # Remove/replace when function is complete
+    icmpSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmpName)
+    sendOnePing(icmpSocket, destinationAddress, ID, ttl)
+    data, addr = receiveOnePing(icmpSocket, ID, timeout, destinationAddress, ttl)
+    icmpSocket.close()
+    return data, addr
 
 
-def ping(host, count, timeout):
-    send = 0
-    lost = 0
-    receive = 0
-    maxTime = 0
-    minTime = 1000
-    sumTime = 0
-    # 1. Look up hostname, resolving it to an IP address
-    desIp = socket.gethostbyname(host)
-    global ID
-    ID = os.getpid()
-    for i in range(0, count):
-        global SEQUENCE
-        SEQUENCE = i
-        # 2. Call doOnePing function, approximately every second
-        delay = doOnePing(desIp, timeout) * 1000
-        send += 1
-        if delay > 0:
-            receive += 1
-            if maxTime < delay:
-                maxTime = delay
-            if minTime > delay:
-                minTime = delay
-            sumTime += delay
-            # 3. Print out the returned delay
-            print("Receive from: " + str(desIp) + ", delay = " + str(int(delay)) + "ms")
+def traceroute(host, timeout):
+    for ttl in range(1, MAX_HOPS):
+        print(ttl, end="\t")
+        # Calculate target IP address
+        destAddr = socket.gethostbyname(host)
+        # Perform the ping at the specified TTL
+        data, addr = doOnePing(destAddr, timeout, ttl)
+        if data == -1:
+            print("*  *  *")
+        elif data == -2:
+            print("Time exceeded")
         else:
-            lost += 1
-            print("Fail to connect. ", end="")
-            if delay == -11:
-                # type = 11, target unreachable
-                print("Target net/host/port/protocol is unreachable.")
-            elif delay == -3:
-                # type = 3, ttl overtime
-                print("Request overtime.")
-            else:
-                # otherwise, overtime
-                print("Request overtime.")
+            print("{:.4f} ms\t{}".format(data * 1000, addr))
+            # Check if the destination is reached
+            if addr == destAddr:
+                break
         time.sleep(1)
-    # 4. Continue this process until stopped
-    if receive != 0:
-        avgTime = sumTime / receive
-        recvRate = receive / send * 100.0
-        print(
-            "\nSend: {0}, success: {1}, lost: {2}, rate of success: {3}%.".format(send, receive, lost, recvRate))
-        print(
-            "MaxTime = {0}ms, MinTime = {1}ms, AvgTime = {2}ms".format(int(maxTime), int(minTime), int(avgTime)))
-    else:
-        print("\nSend: {0}, success: {1}, lost: {2}, rate of success: 0.0%".format(send, receive, lost))
 
 
 if __name__ == '__main__':
     while True:
         try:
             hostName = input("Input ip/name of the host you want: ")
-            count = int(input("How many times you want to detect: "))
             timeout = int(input("Input timeout: "))
-            ping(hostName, count, timeout)
+            traceroute(hostName, timeout)
             break
         except Exception as e:
             print(e)
